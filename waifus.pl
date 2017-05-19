fd864d4b276a2d8746f37f8cfbbe08b721a45bbc
Jose <o.castillojl@gmail.com>
2017-05-19 13:59:17 -0500

    first commit

---
+++ waifus.pl
@@ -0,0 +1,181 @@
+/*Esta es la base de conocimientos de
+chicas de anime, sistema expero
+tu waifu ideal.*/
+
+/* %Base de Chicas
+
+Arqutipo de Chica.*/
+
+ark(hatsune_miku, moe).
+ark(megurine_luka, moe).
+ark(kazami_mizuho, moe).
+ark(shana, tsundere).
+ark(ayuzawa_mizaki, tsundere).
+ark(haruno_sakura, tsundere).
+ark(aisaka_taiga, tsundere).
+ark(kirisaki_chitoge, tsundere).
+ark(langley_asuka, tsundere).
+ark(hino_rei, tsundere).
+ark(konjiki_no_yami, tsundere).
+ark(yuuki_asuna, tsundere).
+ark(francoise_louise, tsundere).
+ark(nakamura_yuri, tsundere).
+ark(nishikino_maki, tsundere).
+ark(hakurei_reimu, tsundere).
+ark(haruhi_suzumiya, tsundere).
+ark(gasai_yuno, yandere).
+ark(ouma_mana, yandere).
+ark(katsura_kotonoha, yandere).
+ark(sonosaki_shion, yandere).
+ark(ryuugu_rena, yangire).
+ark(tokisaki_kurumi, yangire).
+ark(lucy, yangire).
+ark(ayanami_rei, kudere).
+ark(nagato_yuki, kudere).
+ark(misaki_mei, kudere).
+ark(akame, kudere).
+ark(ikaros, kudere).
+ark(konjiki_no_yami, kudere).
+ark(yuzuriha_Inori, kudere).
+ark(akiyama_mio, dandere).
+ark(onodere_kosaki, dandere).
+ark(hyuga_hinata, dandere).
+ark(ichinose_kotomi, dandere).
+ark(shirayuki_mizore, dandere).
+ark(reikadou_ayame, himedere).
+ark(bristol_lilith, himedere).
+ark(yoshino, loli).
+ark(shana, loli).
+ark(gokou_ruri, loli).
+ark(aisaka_taiga, loli).
+ark(shiro, loli).
+
+appears_on(hatsune_miku, vocaloid).
+appears_on(megurine_luka, vocaloid).
+appears_on(shana, shakugan_no_shana).
+appears_on(haruno_sakura, naruto).
+appears_on(aisaka_taiga, toradora).
+appears_on(kirisaki_chitoge, nisekoi).
+appears_on(kazami_mizuho, please_teacher).
+appears_on(reikadou_ayame, ore_no_nounai_sentakushi_ga).
+appears_on(langley_asuka, evangelion).
+appears_on(hino_rei, sailor_moon).
+appears_on(konjiki_no_yami, to_love_ru).
+appears_on(yuuki_asuna, sao).
+appears_on(francoise_louise, zero_no_tsukaima).
+appears_on(nakamura_yuri, angel_beats).
+appears_on(gasai_yuno, mirai_niki).
+appears_on(hakurei_reimu, touhou).
+appears_on(nishikino_maki, love_live).
+appears_on(ouma_mana, guilty_crown).
+appears_on(ayuzawa_mizaki, maid_sama).
+appears_on(katsura_kotonoha, school_days).
+appears_on(sonosaki_shion, higurashi_no_naku).
+appears_on(ryuugu_rena, higurashi_no_naku).
+appears_on(tokisaki_kurumi, date_alive).
+appears_on(lucy, elfen_lied).
+appears_on(ayanami_rei, evangelion).
+appears_on(suzumiya_haruhi, haruhi_no_yuutsu).
+appears_on(nagato_yuki, haruhi_no_yuutsu).
+appears_on(akame, akame_ga_kill).
+appears_on(konjiki_no_nami, to_love_ru).
+appears_on(mizaki_mei, another).
+appears_on(ikaros, sora_no_otoshimono).
+appears_on(yuzuriha_inori, guilty_crown).
+appears_on(akiyama_mio, k-on).
+appears_on(onodere_kosaki, nisekoi).
+appears_on(hyuga_hinata, naruto).
+appears_on(ichinose_kotomi, clannad).
+appears_on(shirayuki_mizore, rosario_vampire).
+appears_on(bristol_lilith, absolute_duo).
+appears_on(yoshino, date_alive).
+appears_on(shiro, no_game_no_life).
+
+/*---------------Color de Cabello--------------------*/
+
+hair(hatsune_miku, light, blue, long).
+hair(megurine_luka, light, pink, long).
+hair(shana, light, red, long).
+hair(hino_rei, dark, brown, long).
+hair(haruno_sakura, light, pink, short).
+hair(aisaka_taiga, light, brown, long).
+hair(kirisaki_chitoge, light, blonde, long).       /*aqui me puse a filosofar si es que existe el rubio oscuro XDDDD*/
+hair(kazami_mizuho, light, red, long).
+hair(reikadou_ayame, light, blonde, long).
+hair(langley_asuka, light, orange, long).
+hair(konjiki_no_yami, light, blonde, long).
+hair(yuuki_asuna, light, brown, long).
+hair(francoise_louise, light, pink, long).
+hair(nakamura_yuri, light, purple, long).
+hair(nishikino_maki, dark, red, long).
+hair(ouma_mana, light, pink, long).
+hair(ayuzawa_mizaki, dark, brown, long).
+hair(katsura_kotonoha, dark, black, long).  
+/*y aqui si existe el negro claro xP */
+hair(sonosaki_shion, light, green, long).
+hair(ryuugu_rena, light, orange, short).
+hair(tokisaki_kurumi, dark, black, long).
+hair(lucy, light, pink, long).
+hair(ayanami_rei, light, blue, short).
+hair(suzumiya_haruhi, dark, brown, long).
+hair(nagato_yuki, light, brown, short).
+hair(akame, dark, black, long).
+hair(mizaki_mei, dark, black, long).
+hair(ikaros, light, pink, short).
+hair(yuzuriha_inori, light, pink, long).
+hair(akiyama_mio, dark, black, long).
+hair(onodere_kosaki, dark, brown, short).
+hair(hyuga_hinata, dark, blue, long).
+hair(ichinose_kotomi, dark, blue, long).
+hair(shirayuki_mizore, light, purple, short).
+hair(bristol_lilith, light, blonde, long).
+hair(yoshino, light, blue, long).
+hair(shiro, light, blue, long).
+
+
+/*---------------------Tipo de ojos------------------*/
+
+eyes(hatsune_miku, light, blue).
+eyes(hino_Rei, dark, purple).
+eyes(shana, light, red).
+eyes(megurine_luka, light, pink).
+eyes(haruno_sakura, light, blue).
+eyes(aisaka_taiga, light, brown).
+eyes(kirisaki_chitoge, light, blue).
+eyes(kazami_mizuho, dark, purple).
+eyes(yuuki_asuna, light, brown).
+eyes(konjiki_no_yami, dark, red).
+eyes(francoise_louise, light, brown).
+eyes(langley_asuka, light, blue).
+eyes(reikadou_ayame, light, yellow).
+eyes(nakamura_yuri, light, blue).
+eyes(nishikino_maki, light, purple).
+eyes(ouma_mana, light, orange).
+eyes(ayuzawa_mizaki, light, brown).
+eyes(katsura_kotonoha, dark, purple).
+eyes(sonosaki_shion, light, green).
+eyes(ryuugu_rena, light, blue).
+eyes(tokisaki_kurumi, two_color, two_color).
+eyes(lucy, light, pink).
+eyes(ayanami_rei, dark, brown).
+eyes(suzumiya_haruhi, dark, brown).
+eyes(nagato_yuki, dark, brown).
+eyes(akame, dark, red).
+eyes(mizaki_mei, dark, brown).
+eyes(ikaros, light, emerald).
+eyes(yuzuriha_inori, light, red).
+eyes(akiyama_mio, dark, black).
+eyes(onodere_kosaki, dark, brown).
+eyes(hyuga_hinata, light, gray).
+eyes(ichinose_kotomi, dark, purple).
+eyes(shirayuki_mizore, light, blue).
+eyes(bristol_lilith, light, blue).
+eyes(yoshino, light, blue).
+eyes(shiro, light, (yellow, red)).
+
+/*-----------------------Reglas-------------------*/
+
+cargar(A):-exist_file(A), consult(A).
+hair_eye_st(X, Y, A, Z, W, S):- hair(S,X,Y,A), eyes(S,Z,W). 
+edad_g(X, Y):- X < 14, Y = loli ; Y = no_loli.
+where_appears(A, X):- appears_on(A, X).
