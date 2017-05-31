809b9d93cc6e9a6aab954478e206e8c19e7679f5
Jose <o.castillojl@gmail.com>
2017-05-31 12:48:07 -0500

    dict_change cambios en if

--- interfazwaifu.java
+++ interfazwaifu.java
@@ -1,11 +1,11 @@
-package PROLOG;
+//package PROLOG;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//package pdef;
+package pdef;
 
 import java.awt.List;
 import java.util.ArrayList;
